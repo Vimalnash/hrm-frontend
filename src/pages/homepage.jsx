24a89e1197628743e14659ec4369b84d74eb1813
vimalnash <vimalnash@gmail.com>
2025-07-25 00:28:33 +0530

    MinorChange

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,7 +3,7 @@ import { PageTitle } from "../components/pagetitle";
 export function HomePage() {
   return (
     <>
-      <PageTitle><p>Welcome to HRM</p></PageTitle>
+      <PageTitle><p>Welcome to HRM-Attendance and LabourShift</p></PageTitle>
       <div className="min-h-full mt-10 border-2 px-2 md:px-32 flex flex-col justify-center gap-4">
         Here You can mark attendance for labours and assign shift
       </div>
